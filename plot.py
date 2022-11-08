10a91776dbb8456d42ce54f14b13545ba85a9b27
Yihong Zhang <yz489@cs.washington.edu>
2022-11-08 15:05:23 -0800

    tweaks

--- plot.py
+++ plot.py
@@ -7,8 +7,9 @@ with open('benchmarks.csv', newline='') as csvfile:
     reader = csv.reader(csvfile)
     for row in reader:
         data.append((int(row[0][9:]), row[1], float(row[2]), float(row[3])))
-X = 3
 
+X = 3
+plt.figure(1)
 egglog = list(map(lambda x: (x[X], x[2]/1e9), filter(lambda x: x[1] == 'Egglog', data)))
 # egglog = sorted(egglog, key = lambda x: x[1])
 egglog_x = list(map(lambda x:x[0], egglog))
@@ -25,8 +26,31 @@ egglog = list(map(lambda x: (x[X], x[2]/1e9), filter(lambda x: x[1] == 'EgglogNa
 # egglog = sorted(egglog, key = lambda x: x[1])
 egglog_x = list(map(lambda x:x[0], egglog))
 egglog_y = list(map(lambda x:x[1], egglog))
-plt.plot(egglog_y, egglog_x, label="egglog-SN")
-
+plt.plot(egglog_y, egglog_x, label="egglog-naive")
 plt.legend(loc='upper right')
-plt.savefig("plot.png")
+plt.savefig("nodes-time.png")
+
+
+plt.figure(2)
+X = 0
+egglog = list(map(lambda x: (x[X], x[2]/1e9), filter(lambda x: x[1] == 'Egglog', data)))
+# egglog = sorted(egglog, key = lambda x: x[1])
+egglog_x = list(map(lambda x:x[0], egglog))
+egglog_y = list(map(lambda x:x[1], egglog))
+plt.plot(egglog_x, egglog_y, label="egglog")
+
+egg = list(map(lambda x: (x[X], x[2]/1e9), filter(lambda x: x[1] == 'Egg', data)))
+# egg = sorted(egg, key = lambda x: x[1])
+egg_x = list(map(lambda x:x[0], egg))
+egg_y = list(map(lambda x:x[1], egg))
+plt.plot(egg_x, egg_y, label="egg")
+
+# egglog = list(map(lambda x: (x[X], x[2]/1e9), filter(lambda x: x[1] == 'EgglogNaive', data)))
+# # egglog = sorted(egglog, key = lambda x: x[1])
+# egglog_x = list(map(lambda x:x[0], egglog))
+# egglog_y = list(map(lambda x:x[1], egglog))
+# plt.plot(egglog_x, egglog_y, label="egglog-naive")
+plt.legend(loc='upper right')
+plt.savefig("time-iter.png")
+
 plt.show()
